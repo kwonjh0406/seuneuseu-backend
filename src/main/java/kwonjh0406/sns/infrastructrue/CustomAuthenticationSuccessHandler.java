12d2bb86dc6e1fc0e2642a552ad0cb87982d89d5
kwonjh0406 <kwonjh0406@gmail.com>
2025-01-13 21:31:45 +0900

    Session Controller 및 서비스 리팩토링

--- src/main/java/kwonjh0406/sns/infrastructrue/CustomAuthenticationSuccessHandler.java
+++ src/main/java/kwonjh0406/sns/infrastructrue/CustomAuthenticationSuccessHandler.java
@@ -3,6 +3,7 @@ package kwonjh0406.sns.infrastructrue;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kwonjh0406.sns.oauth2.dto.CustomOAuth2User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -12,8 +13,12 @@ import java.io.IOException;
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
+    @Value("${base.url}")
+    private String baseUrl;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+
 
         Object principal = authentication.getPrincipal();
 
@@ -22,12 +27,12 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
             if (oAuth2User.getUser().getIsNew()) { // 사용자가 아직 초기 설정을 안한 사용자인 경우
 
                 System.out.println(oAuth2User.getUser().getUsername());
-                response.sendRedirect("http://localhost:3000/welcome"); // 초기 설정 페이지로 리다이렉션
+                response.sendRedirect(baseUrl + "/welcome"); // 초기 설정 페이지로 리다이렉션
                 return;
             }
         }
 
-        response.sendRedirect("http://localhost:3000/");
+        response.sendRedirect(baseUrl);
     }
 }
 
