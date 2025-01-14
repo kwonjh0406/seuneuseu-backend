c158c79bd810073c3e1093755eb4319a82a1335a
kwonjh0406 <kwonjh0406@gmail.com>
2025-01-14 16:03:37 +0900

    fix

---
+++ src/main/java/kwonjh0406/sns/infrastructrue/HttpSessionListener.java
@@ -0,0 +1,18 @@
+package kwonjh0406.sns.infrastructrue;
+
+import jakarta.servlet.http.HttpSessionEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HttpSessionListener implements jakarta.servlet.http.HttpSessionListener {
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        System.out.println("Session Created: " + se.getSession().getId());
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        System.out.println("Session Destroyed: " + se.getSession().getId());
+    }
+}
