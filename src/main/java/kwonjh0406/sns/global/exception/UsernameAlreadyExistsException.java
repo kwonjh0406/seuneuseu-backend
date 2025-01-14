500794a66318266a4b7e15800beaf3167784c781
kwonjh0406 <kwonjh0406@gmail.com>
2025-01-15 04:00:33 +0900

    feat

---
+++ src/main/java/kwonjh0406/sns/global/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package kwonjh0406.sns.global.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
