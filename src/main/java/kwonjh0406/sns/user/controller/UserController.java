606cffab0e43a59e48372d3b01371e14b0b3c45d
kwonjh0406 <kwonjh0406@gmail.com>
2025-02-02 00:08:52 +0900

    refactor: security 허용 경로 설정 및, 사용자 api 쪽 url 경로 restful 수정

--- src/main/java/kwonjh0406/sns/user/controller/UserController.java
+++ src/main/java/kwonjh0406/sns/user/controller/UserController.java
@@ -4,16 +4,14 @@ import jakarta.validation.Valid;
 import kwonjh0406.sns.global.dto.ApiResponse;
 import kwonjh0406.sns.user.dto.*;
 import kwonjh0406.sns.user.service.UserService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
@@ -40,50 +38,11 @@ public class UserController {
         }
     }
 
-    @GetMapping("/profile/{username}")
+    @GetMapping("/{username}/profile")
     public ResponseEntity<UserProfileResponse> getUser(@PathVariable String username) {
         UserProfileResponse userProfileResponse = userService.getUserProfileByUsername(username);
         return ResponseEntity.ok(userProfileResponse);
     }
 
-    @GetMapping("/me/profile")
-    public ResponseEntity<ApiResponse<ProfileEditResponse>> getMyProfile() {
-        try {
-            ProfileEditResponse profileEditResponse = userService.getProfileEdit();
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(
-                            ApiResponse.<ProfileEditResponse>builder()
-                                    .message(null)
-                                    .data(profileEditResponse)
-                                    .build()
-                    );
 
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(
-                            ApiResponse.<ProfileEditResponse>builder()
-                                    .message(null)
-                                    .data(null)
-                                    .build()
-                    );
-        }
-    }
-
-    @PatchMapping("/me/profile")
-    public ResponseEntity<ApiResponse<Void>> profileEdit(@Valid ProfileEditRequest profileEditRequest) {
-        try {
-            userService.editProfile(profileEditRequest);
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(
-                            ApiResponse.<Void>builder()
-                                    .build());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(
-                            ApiResponse.<Void>builder()
-                                    .message(e.getMessage())
-                                    .build()
-                    );
-        }
-    }
 }
--- src/main/java/kwonjh0406/sns/user/controller/UserSearchController.java
+++ src/main/java/kwonjh0406/sns/user/controller/UserSearchController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/user/search")
+@RequestMapping("/api/users/search")
 @RequiredArgsConstructor
 public class UserSearchController {
 
