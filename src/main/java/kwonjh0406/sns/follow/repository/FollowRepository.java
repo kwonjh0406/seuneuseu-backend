e7156e9e9d6b36e880a6fb1c0bbbfc66582c775f
kwonjh0406 <kwonjh0406@gmail.com>
2025-02-04 11:02:17 +0900

    feat: 팔로잉 게시글 조회 초안

--- src/main/java/kwonjh0406/sns/follow/repository/FollowRepository.java
+++ src/main/java/kwonjh0406/sns/follow/repository/FollowRepository.java
@@ -5,8 +5,11 @@ import kwonjh0406.sns.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     boolean existsByFollowerAndFollowing(User follower, User following);
     void deleteByFollowerAndFollowing(User follower, User following);
+    List<Follow> findByFollower(User user);
 }
--- src/main/java/kwonjh0406/sns/follow/service/FollowService.java
+++ src/main/java/kwonjh0406/sns/follow/service/FollowService.java
@@ -5,11 +5,23 @@ import kwonjh0406.sns.follow.dto.FollowCheck;
 import kwonjh0406.sns.follow.entity.Follow;
 import kwonjh0406.sns.follow.repository.FollowRepository;
 import kwonjh0406.sns.oauth2.dto.CustomOAuth2User;
+import kwonjh0406.sns.post.dto.PageRequestDto;
+import kwonjh0406.sns.post.dto.PostResponse;
+import kwonjh0406.sns.post.entity.Post;
+import kwonjh0406.sns.post.entity.PostImage;
+import kwonjh0406.sns.post.repository.PostRepository;
 import kwonjh0406.sns.user.entity.User;
 import kwonjh0406.sns.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +29,7 @@ public class FollowService {
 
     private final UserRepository userRepository;
     private final FollowRepository followRepository;
+    private final PostRepository postRepository;
 
     public FollowCheck isFollow(String username) {
         FollowCheck followCheck = new FollowCheck();
@@ -66,5 +79,27 @@ public class FollowService {
             userRepository.save(following);
             userRepository.save(follower);
         }
+    }
+
+
+    public List<PostResponse> getFollowingPosts() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof CustomOAuth2User oAuth2User) {
+            List<Follow> followingList = followRepository.findByFollower(oAuth2User.getUser());
+            List<User> followingUserList = followingList.stream()
+                    .map(Follow::getFollowing)
+                    .collect(Collectors.toList());
+            List<Post> posts = postRepository.findPostsByFollowingWithPage(followingUserList);
+            return posts.stream()
+                    .map(post -> new PostResponse(
+                            post,
+                            post.getPostImages().stream()
+                                    .map(PostImage::getImageUrl)
+                                    .collect(Collectors.toList())
+                    ))
+                    .collect(Collectors.toList());
+        }
+        return null;
     }
 }
